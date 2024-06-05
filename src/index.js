ed2d7431a1573f773da47df57c20f38e36c9683c
Thanh Thang <phanthanhthangdtht123@gmail.com>
2024-06-05 09:43:47 +0700

    new

--- src/index.js
+++ src/index.js
@@ -129,7 +129,12 @@ app.post('/login', async (req, res) => {
             return res.status(401).render('login', { message: 'Mật khẩu không chính xác!' });
         }
         // Nếu mọi thứ ổn, chuyển hướng người dùng tới trang chủ
-        return res.redirect('/');
+        else{
+            return res.render('home',{
+                user: user,
+                // data: result.rows
+            });
+        }
     } catch (error) {
         console.error('Login error:', error);
         res.status(500).render('login', { message: 'Lỗi server khi đăng nhập!' });
@@ -142,9 +147,17 @@ app.get('/', (req, res) => {
             console.error(err);
             res.status(500).send('Internal Server Error');
         } else {
-            res.render('home', {
-                data: result.rows
-            });
+            console.log(req.session); // Kiểm tra xem bên trong req.session có gì
+            if (req.session && req.session.user) {
+                res.render('home', {
+                    user: req.session.user,
+                    data: result.rows
+                });
+            } else {
+                res.render('home', {
+                    data: result.rows
+                });
+            }
         }
     });
 });
